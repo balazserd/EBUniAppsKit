d0507d1fa71b1a35250b768aeb2be6bdb63e6e4b
Balazs Erdesz <ispain17@yahoo.com>
2023-08-09 19:09:55 +0200

    add throwingtaskgroup to implementation

--- Sources/EBUniAppsKit/Array+Extensions.swift
+++ Sources/EBUniAppsKit/Array+Extensions.swift
@@ -10,32 +10,35 @@ import Foundation
 public extension Array {
     /// Concurrently processes the elements of an array.
     ///
-    /// - Parameter group: The `ThrowingTaskGroup` which will handle the concurrent async processing of elements.
     /// - Parameter maxConcurrencyCount: The number of maximum possible concurrent processing the `ThrowingTaskGroup` should handle.
     /// - Parameter processBlock: The async operation to perform on each element in the array.
-    func mapAsync<T, Failure: Error>(in group: inout ThrowingTaskGroup<T, Failure>,
-                                     maxConcurrencyCount: Int = .max,
-                                     _ processBlock: @escaping (Self.Element) async throws -> T) async throws -> [T] {
-        var returnArray = [T]()
-        
-        for index in 0..<Swift.min(maxConcurrencyCount, self.count) {
-            group.addTask {
-                return try await processBlock(self[index])
-            }
-        }
-        
-        var nextIndex = maxConcurrencyCount
-        while let nextElement = try await group.next() {
-            if nextIndex < self.count {
-                group.addTask { [nextIndex] in
-                    return try await processBlock(self[nextIndex])
+    ///
+    /// The underlying implementation uses a ``Swift.ThrowingTaskGroup``.
+    func mapAsync<T>(maxConcurrencyCount: Int = .max,
+                     _ mapBlock: @escaping (Self.Element) async throws -> T)
+    async throws -> [T] {
+        try await withThrowingTaskGroup(of: T.self) { group in
+            var returnArray = [T]()
+            
+            for index in 0..<Swift.min(maxConcurrencyCount, self.count) {
+                group.addTask {
+                    return try await mapBlock(self[index])
                 }
             }
             
-            nextIndex += 1
-            returnArray.append(nextElement)
+            var nextIndex = maxConcurrencyCount
+            while let nextElement = try await group.next() {
+                if nextIndex < self.count {
+                    group.addTask { [nextIndex] in
+                        return try await mapBlock(self[nextIndex])
+                    }
+                }
+                
+                nextIndex += 1
+                returnArray.append(nextElement)
+            }
+            
+            return returnArray
         }
-        
-        return returnArray
     }
 }
