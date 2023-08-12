e78c01a6e576aae7d0b8269af58bb71829b415b5
Balazs Erdesz <ispain17@yahoo.com>
2023-08-12 11:51:44 +0200

    Add `mapConcurrentlyThenPerformSeriallyAsync` function

--- Sources/EBUniAppsKit/Array+Extensions.swift
+++ Sources/EBUniAppsKit/Array+Extensions.swift
@@ -70,4 +70,57 @@ public extension Array {
             }
         }
     }
+    
+    /// Concurrently processes the elements of an array, then performs an operation with the results serially, preserving the original array's order.
+    ///
+    /// - Parameter maxConcurrencyCount: The number of maximum possible concurrent processing the `ThrowingTaskGroup` should handle.
+    /// - Parameter mapBlock: The async operation to perform for each element in the array.
+    ///
+    /// The underlying implementation uses a ``Swift.ThrowingTaskGroup``.
+    ///
+    /// This function is useful when the last part of an expensive operation must be performed serially, but inputs for the serial part can be obtained concurrently.
+    ///
+    /// The process will maintain an input result buffer, always waiting for the next-in-order input to be received but starting new processes if later input result arrive earlier - up until the concurrency limit.
+    ///
+    /// The following errors can be thrown:
+    /// | Code | Description                      |
+    /// | ---- | -------------------------------- |
+    /// | -1   | The buffer element is not found. |
+    func mapConcurrentlyThenPerformSeriallyAsync<M>(maxConcurrencyCount: Int = .max,
+                                                    mapPriority: TaskPriority = .medium,
+                                                    _ mapBlock: @escaping (Self.Element) async throws -> M,
+                                                    _ serialPerformBlock: @escaping (M) async throws -> Void)
+    async throws {
+        try await withThrowingTaskGroup(of: (Int, M).self) { group in
+            for index in 0..<Swift.min(maxConcurrencyCount, self.count) {
+                group.addTask(priority: mapPriority) {
+                    return try await (index, mapBlock(self[index]))
+                }
+            }
+            
+            var nextTaskIndex = maxConcurrencyCount
+            var buffer = [(Int, M)]()
+            for nextResultIndex in 0..<self.count {
+                repeat {
+                    guard let (newResultIndex, newResult) = try await group.next() else { break }
+                    buffer.append((newResultIndex, newResult))
+                    
+                    if nextTaskIndex < self.count {
+                        group.addTask(priority: mapPriority) { [nextTaskIndex] in
+                            return try await (nextTaskIndex, mapBlock(self[nextTaskIndex]))
+                        }
+                    }
+                    
+                    nextTaskIndex += 1
+                } while buffer.first(where: { $0.0 == nextResultIndex }) == nil
+                
+                guard let nextResultBufferIndex = buffer.firstIndex(where: { $0.0 == nextResultIndex }) else {
+                    throw NSError(domain: "EBUniAppsKit", code: -1)
+                }
+                
+                try await serialPerformBlock(buffer[nextResultBufferIndex].1)
+                buffer.remove(at: nextResultBufferIndex)
+            }
+        }
+    }
 }
